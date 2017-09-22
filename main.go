bdbd9dbdd05bf014d42ca7f4af13ac05e2d84726
Naoto Kaneko <naoty.k@gmail.com>
2017-09-22 18:47:19 +0900

    Start a HTTP server

--- main.go
+++ main.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+
+	"github.com/naoty/refrigerator/handlers"
 )
 
 func main() {
 	fmt.Println("Hello, world!")
+
+	http.Handle("/foods", &handlers.FoodsHandler{})
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	os.Exit(0)
 }
