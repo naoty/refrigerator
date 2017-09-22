120baa5d417d189565ca02d591cd52cbaa9408e3
Naoto Kaneko <naoty.k@gmail.com>
2017-09-22 18:54:25 +0900

    Add tags to Food for json

--- models/food.go
+++ models/food.go
@@ -5,23 +5,23 @@ import "time"
 // Food is a food to be stored in the refrigerator.
 type Food struct {
 	// Name is the name of the food.
-	Name string
+	Name string `json:"name"`
 
 	// Quantity is the quantity of the food.
-	Quantity Quantity
+	Quantity Quantity `json:"quantity"`
 
 	// UseByDate is the use-by-date of the food.
-	UseByDate time.Time
+	UseByDate time.Time `json:"useByDate"`
 
 	// BestByDate is the best-by-date of the food.
-	BestByDate time.Time
+	BestByDate time.Time `json:"bestByDate"`
 }
 
 // Quantity is the quantity used to represent the quantity of foods.
 type Quantity struct {
 	// Value is the value of quantity.
-	Value int
+	Value int `json:"value"`
 
 	// Unit is the unit of quantity.
-	Unit string
+	Unit string `json:"unit"`
 }
