6da900d4dbcafc77dc404703e9f64cd030361c1d
Naoto Kaneko <naoty.k@gmail.com>
2017-09-22 20:59:11 +0900

    Return foods as JSON at /foods

--- handlers/foods_handler.go
+++ handlers/foods_handler.go
@@ -1,8 +1,11 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/naoty/refrigerator/models"
 )
 
 // FoodsHandler is a HTTP handler for foods.
@@ -10,6 +13,16 @@ type FoodsHandler struct {
 }
 
 func (h *FoodsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("TODO: Return foods")
+	// TODO: Get data from database
+	foods := []models.Food{
+		models.Food{Name: "Apple", Quantity: models.Quantity{Value: 1}},
+		models.Food{Name: "Orange", Quantity: models.Quantity{Value: 2}},
+	}
+	data, err := json.Marshal(foods)
+	if err != nil {
+		fmt.Println("TODO: error handling")
+	}
+
 	w.WriteHeader(http.StatusOK)
+	w.Write(data)
 }
